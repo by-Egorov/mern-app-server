a73010ea82b582dcd80425a6fbf3b5e43e1059b7
EgorovAlexandr <egorov.dev@gmail.com>
2023-11-18 13:04:51 +0300

    добавление в избранное/корзину

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001
 // Подключение к ДБ
 mongoose
   .connect(process.env.MONGODB_URI)
-  
+
   .then(() => console.log('DB ok'))
   .catch(() => console.log('DB error', err))
 // Создание сервера
@@ -27,8 +27,7 @@ app.use('/api', authRouter)
 app.get('/api/user/me', authMiddleware, async (req, res) => {
   try {
     const user = await User.findById(req.user.id)
-    return res.json({user})
-
+    return res.json({ user })
   } catch (e) {
     console.log(e)
     res.status(400).json({
@@ -43,13 +42,57 @@ app.get('/api/products', async (req, res) => {
 
     return res.json({ products })
   } catch (error) {
-    console.error('Ошибка при получении товаров:', error)
-    res.status(500).json({ message: 'Произошла ошибка при получении товаров' })
+    console.error('Ошибка при получении продуктов:', error)
+    res
+      .status(500)
+      .json({ message: 'Произошла ошибка при получении продуктов' })
   }
 })
 
+// http://localhost:5000/api/products/:id
+// app.get('/api/products/:id', async (req, res) => {
+//   try {
+//     const product = await Product.findById(req.params.id)
+//     res.json({ product })
+//   } catch (error) {
+//     console.error('Ошибка при получении продукта:', error)
+//     res.status(500).json({ message: 'Произошла ошибка при получении продукта' })
+//   }
+// })
+
+// http://localhost:5000/api/products/:id
+// app.delete('/api/products/cart/delete', authMiddleware, async (req, res) => {
+//   try {
+//     const productId = req.body.productId
+//     const userId = req.user.id
+//     const user = await User.findById(userId)
+//     console.log(user)
+
+//     if (!user) {
+//       console.log('Пользователь не найден')
+//       return res.status(404).json({ message: 'Пользователь не найден' })
+//     }
+
+//     const product = await Product.findById(productId)
+//     console.log(product)
+//     if (!product) {
+//       return res.json({ message: 'Такого продукта не существует' })
+//     }
+//     await User.findByIdAndUpdate(
+//       user._id,
+//       {
+//         $pull: { cart: product._id },
+//       },
+//       { new: true }
+//     )
+//   } catch (error) {
+//     console.error('Ошибка удаления продукта:', error)
+//     res.status(500).json({ message: 'Произошла ошибка удаления продукта' })
+//   }
+// })
+
 // http://localhost:5000/api/products/add
-app.post('/api/products/add', roleMiddleware(["ADMIN"]), async (req, res) => {
+app.post('/api/products/add', roleMiddleware(['ADMIN']), async (req, res) => {
   try {
     const { title, category, description, price, image } = req.body
     const doc = new Product({
@@ -76,7 +119,6 @@ app.post('/api/products/add', roleMiddleware(["ADMIN"]), async (req, res) => {
 app.get('/api/products/favorite', authMiddleware, async (req, res) => {
   try {
     const user = await User.findById(req.user.id)
-
     const list = await Promise.all(
       user.favorite.map((product) => {
         return Product.findById(product._id)
@@ -148,7 +190,6 @@ app.post('/api/products/favorite/add', authMiddleware, async (req, res) => {
 
 // http://localhost:5000/api/products/cart/add
 app.post('/api/products/cart/add', authMiddleware, async (req, res) => {
-
   try {
     const productId = req.body.productId
     const userId = req.user.id
@@ -167,19 +208,55 @@ app.post('/api/products/cart/add', authMiddleware, async (req, res) => {
     }
 
     await User.findByIdAndUpdate(
-        user._id,
-        {
-          $addToSet: { cart: product._id },
-        },
-        { new: true }
+      user._id,
+      {
+        $addToSet: { cart: product._id },
+      },
+      { new: true }
     )
     console.log('Продукт успешно добавлен в корзину')
     res.status(200).json({ message: 'Продукт успешно добавлен в корзину' })
   } catch (error) {
     console.error('Ошибка при добавлении продукта в корзину:', error)
     res
-        .status(500)
-        .json({ message: 'Произошла ошибка при добавлении продукта в корзину' })
+      .status(500)
+      .json({ message: 'Произошла ошибка при добавлении продукта в корзину' })
+  }
+})
+
+// http://localhost:5000/api/products/cart/delete
+app.delete('/api/products/cart/delete', authMiddleware, async (req, res) => {
+  try {
+    const productId = req.body.productId
+    const userId = req.user.id
+    const user = await User.findById(userId)
+
+    if (!user) {
+      console.log('Пользователь не найден')
+      return res.status(404).json({ message: 'Пользователь не найден' })
+    }
+
+    const product = await Product.findById(productId)
+
+    if (!product) {
+      console.log('Продукт не найден')
+      return res.status(404).json({ message: 'Продукт не найден' })
+    }
+
+    await User.findByIdAndUpdate(
+      user._id,
+      {
+        $pull: { cart: product._id },
+      },
+      { new: true }
+    )
+    console.log('Продукт успешно добавлен в корзину')
+    res.status(200).json({ message: 'Продукт успешно добавлен в корзину' })
+  } catch (error) {
+    console.error('Ошибка при добавлении продукта в корзину:', error)
+    res
+      .status(500)
+      .json({ message: 'Произошла ошибка при добавлении продукта в корзину' })
   }
 })
 
